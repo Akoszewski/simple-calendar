58f00bb22bf9fcc3a4d9c42567ad90c73608ed15
AKoszewski <est@onet.com.pl>
2020-03-23 20:30:17 +0100

    Delete unnecessary dependences

--- src/main.js
+++ src/main.js
@@ -2,15 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import FullCalendar from 'vue-full-calendar'
 import 'fullcalendar/dist/fullcalendar.css'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'balloon-css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.use(FullCalendar)
-Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
